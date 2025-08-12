0e0944ee3fb4b6f3b2941c7c004d70ea1866e15f
hagios <agbajeabiodunkay@gmail.com>
2025-08-12 15:22:38 +0100

    first push to main branch

---
+++ client/src/assets/index.ts
@@ -0,0 +1,73 @@
+import slide1 from "./Images/slide1.svg";
+import slide2 from "./Images/slide2.svg";
+import slide3 from "./Images/slide3.svg";
+import slide4 from "./Images/slide4.svg";
+import slide5 from "./Images/slide5.svg";
+import slide6 from "./Images/slide6.svg";
+import slide7 from "./Images/slide7.svg";
+import slide8 from "./Images/slide8.svg";
+import slide9 from "./Images/slide9.svg";
+import slide10 from "./Images/slide10.svg";
+import slide11 from "./Images/slide11.svg";
+import slide12 from "./Images/slide12.svg";
+import slide13 from "./Images/slide13.svg";
+import slide14 from "./Images/slide14.svg";
+import slide15 from "./Images/slide15.svg";
+import slide16 from "./Images/slide16.svg";
+import slide17 from "./Images/slide17.svg";
+import slide18 from "./Images/slide18.svg";
+import slide19 from "./Images/slide19.svg";
+import slide20 from "./Images/slide20.svg";
+import slide21 from "./Images/slide21.svg";
+import slide22 from "./Images/slide22.svg";
+import slide23 from "./Images/slide23.svg";
+import slide24 from "./Images/slide24.svg";
+import slide25 from "./Images/slide25.svg";
+import slide26 from "./Images/slide26.svg";
+import slide27 from "./Images/slide27.svg";
+import slide28 from "./Images/slide28.svg";
+import slide29 from "./Images/slide29.svg";
+import slide30 from "./Images/slide30.svg";
+import slide31 from "./Images/slide31.svg";
+import slide32 from "./Images/slide32.svg";
+import slide33 from "./Images/slide33.svg";
+import slide34 from "./Images/slide34.svg";
+import slide35 from "./Images/slide35.svg";
+
+export {
+  slide1,
+  slide2,
+  slide3,
+  slide4,
+  slide5,
+  slide6,
+  slide7,
+  slide8,
+  slide9,
+  slide10,
+  slide11,
+  slide12,
+  slide13,
+  slide14,
+  slide15,
+  slide16,
+  slide17,
+  slide18,
+  slide19,
+  slide20,
+  slide21,
+  slide22,
+  slide23,
+  slide24,
+  slide25,
+  slide26,
+  slide27,
+  slide28,
+  slide29,
+  slide30,
+  slide31,
+  slide32,
+  slide33,
+  slide34,
+  slide35,
+};
