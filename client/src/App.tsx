56798c6dab5180950ba9ef76b1f31ad9d26170c3
hagios <agbajeabiodunkay@gmail.com>
2025-08-19 02:21:15 +0100

    first push to main branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Splash } from "./components";
+import { Splash, Login, Signup } from "./components";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Splash />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
     },
   ]);
 
